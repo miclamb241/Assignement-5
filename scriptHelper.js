8c3d906d3adeeeea9924f41f0056d32150f83b0e
miclamb241 <Zachlambertstl@gmail.com>
2021-04-09 15:21:38 -0500

    Hello

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,48 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+
+    if(testInput === "")
+    {
+      return "Empty";
+    }
+    else
+      {
+        testInput = parseInt(testInput);
+
+        if(isNaN(testInput) === true)
+            {
+                return "Not a number";
+            }
+            else if(isNaN(testInput) === false)
+                {
+                    return "Is a number";
+                }
+      }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+
+    window.addEventListener("load", function(){
+        let form = document.getElementbyId("testForm");
+        form.addEventListener("submit", function(event){
+            let pilotName = document.querySelector("input[name=pilotName]");
+            let copilotName = document.querySelector("input[name=copilotName]");
+            fuelLevelName = document.querySelector("input[name=fuelLevel]");
+            cargoLevelName = document.querySelector("input[name=cargoMass]");
+
+            if(pilotName.value === "" || copilotName.value === "" || fuelLevelName.value === "" || cargoLevelName.value === ""){
+                window.alert("All fields are required!");
+                event.preventDefault();            
+            }
+            else if(validateInput(pilotName.value) !== "Not a number" || validateInput(copilotName.value) !== "Not a number" || validateInput(fuelLevelName.value) !== "Is a number" || validateInput(cargoLevelName.value) !== "Is a number")
+                {
+                    window.alert("The information provided is invalid");
+                    event.preventDefault();
+                }
+        });
+    });
+    
 }
 
 async function myFetch() {
