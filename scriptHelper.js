a56fa2752cad082fdb3b4a4548f9905ccf7e3381
miclamb241 <Zachlambertstl@gmail.com>
2021-04-15 22:52:24 -0500

    had stuff commented out

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,5 +1,5 @@
 // Write your helper functions here!
-//require('isomorphic-fetch');
+require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
@@ -116,8 +116,8 @@ function pickPlanet(planets) {
     return planets[random];
 }
 
-//module.exports.addDestinationInfo = addDestinationInfo;
-//module.exports.validateInput = validateInput;
-//module.exports.formSubmission = formSubmission;
-//module.exports.pickPlanet = pickPlanet; 
-//module.exports.myFetch = myFetch;
+module.exports.addDestinationInfo = addDestinationInfo;
+module.exports.validateInput = validateInput;
+module.exports.formSubmission = formSubmission;
+module.exports.pickPlanet = pickPlanet; 
+module.exports.myFetch = myFetch;
