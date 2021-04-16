756b97df3ce55b3b17f7ef4a2554dae0c1ed0a9b
miclamb241 <Zachlambertstl@gmail.com>
2021-04-16 03:00:11 -0500

    back to the start

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const { formSubmission } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
 
+    let document = window.document;
+    let faultyItems = document.querySelector("#faultyItems");
+    let pilotName = document.querySelector("input[name=pilotName]");
+    let copilotName = document.querySelector("input[name=copilotName]");
+    let fuelLevel = document.querySelector("input[name=fuelLevel]");
+    let cargoMass = document.querySelector("input[name=cargoMass]");
+
+    formSubmission(document, faultyItems, pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value);
+
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse = myFetch();
